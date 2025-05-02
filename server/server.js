f181c31544ab3b2abfc87607dc0a0681918f38da
Deepak Kumar <i.deepak0808@gmail.com>
2025-05-02 10:11:15 +0530

    update

--- server/server.js
+++ server/server.js
@@ -4,20 +4,43 @@ const connectDB = require("./config/db");
 require("dotenv").config();
 const cors = require("cors");
 
-// Allowlisted frontend origins 
+// // Allowlisted frontend origins 
+// const allowedOrigins = [
+//   "http://localhost:3000",             
+//   "https://job-listing-app-rho.vercel.app",
+// ];
+
+// // CORS configuration
+// app.use(
+//   cors({
+//     origin: function (origin, callback) {
+//       if (!origin || allowedOrigins.includes(origin)) {
+//         callback(null, true);
+//       } else {
+//         callback(new Error("Not allowed by CORS"));
+//       }
+//     },
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     credentials: true,
+//   })
+// );
+
+
 const allowedOrigins = [
-  "http://localhost:3000",             
+  "http://localhost:3000",
   "https://job-listing-app-rho.vercel.app",
+  "https://job-listing-app-rho.vercel.app/",
+
 ];
 
-// CORS configuration
+
 app.use(
   cors({
     origin: function (origin, callback) {
       if (!origin || allowedOrigins.includes(origin)) {
         callback(null, true);
       } else {
-        callback(new Error("Not allowed by CORS"));
+        callback(new Error("CORS not allowed"));
       }
     },
     methods: ["GET", "POST", "PUT", "DELETE"],
